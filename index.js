42cb8463d28fa9bacd0beabb745c9e4b9590805f
Erick Vargas <erickkvargas@gmail.com>
2022-04-04 15:52:32 -0700

    commit tree 21200d065a088e27c51ff87b067e391e8fe7d48d

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  
+  if (ride <= 400)
+  {
+    let result = "This one is on me!";
+    return result;
+  }
+  else if (ride >400 && ride <2000)
+  {
+    let cost = "That will be twenty bucks.";
+    return cost;
+  }
+  else if (ride > 2000 && ride < 2500)
+  {
+    let cost2 = "I will gladly take your thirty bucks.";
+    return cost2;
+  }
+  else ( ride > 2500)
+  {
+    let cost3 = "No can do.";
+    return cost3;
+  }
+}
+
+function ternaryCheckCity(city){
+  city === "NYC"? (city ="Ok, sounds good."):(city ="No go.");
+  return city;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip)
+{
+  switch (tip)
+  {
+    case "generous": 
+    return "Thank you so much.";
+    break;
+
+    case "not generous": 
+    return "Thank you.";
+    break;
+    default: return "Bye.";
+  }
+  
   // Write your code here!
 }
